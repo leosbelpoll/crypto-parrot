cc8044a94b00e15b2111b0a0fb25c1c83cfdc14e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-04-24 20:33:33 +0000

    Deploy leosbelpoll/crypto-parrot to github.com/leosbelpoll/crypto-parrot.git:gh-pages

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders main title link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Crypto-parrot client/i);
-  expect(linkElement).toBeInTheDocument();
-});
